bad4a4e00f19d06741668c36337eb00f80911c73
hirak1984 <hirak1984@gmail.com>
2020-05-04 14:53:30 -0400

    separate folder for production website

---
+++ www/build/precache-manifest.d4b3bdd18887f4578035bfd7a54a7f6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc9465058251169deb1761af5317a65",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "90d15065d998aa42ef9a",
+    "url": "/react/static/css/2.f681e662.chunk.css"
+  },
+  {
+    "revision": "919244ae062c28398a55",
+    "url": "/react/static/css/main.ab0dbed2.chunk.css"
+  },
+  {
+    "revision": "90d15065d998aa42ef9a",
+    "url": "/react/static/js/2.3e5d634b.chunk.js"
+  },
+  {
+    "revision": "5e819de06af026d7499024f710cfdda3",
+    "url": "/react/static/js/2.3e5d634b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "919244ae062c28398a55",
+    "url": "/react/static/js/main.2f43f4f3.chunk.js"
+  },
+  {
+    "revision": "e0581a06f7abc376e506",
+    "url": "/react/static/js/runtime-main.b9b72fc1.js"
+  }
+]);
